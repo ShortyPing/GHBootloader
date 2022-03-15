5f0adf783de9830d75d1bd39500c41fec218bdc2
Michael Steinmötzger <m.steinmoetzger@gmail.com>
2022-03-15 11:44:19 +0100

    fixed #1

--- asm/boot.asm
+++ asm/boot.asm
@@ -1,53 +1,63 @@
-;; set video mode
-mov ah, 0x0
-mov al, 0x3 ; video mode (16 color text)
-int 0x10
-;; set background color to blue (1)
-mov ah, 0xB ; instruction number
-mov bh, 0x0 ; idk why its there but it has to be there
-mov bl, 0x1 ; background color register 0x1 .. blue
-int 0x10 ; interrupt call
+[bits 16]
+[org 0x7C00]
 
-mov si, str
-call print
+;; Setup the graphics
+Setup:
+	;; Set video mode
+	mov ah, 0x0
+	mov al, 0x3
+	int 0x10
 
-
-jmp $
-
-print:
-	mov al, 0
-
-.endl:
-	call mvc
-
-.loop:
-	lodsb
-	cmp al, 0
-	je .return
-	mov ah, 0x9
+	;; Set background color
+	mov ah, 0xB
 	mov bh, 0x0
-	mov bl, 0xF
-	mov cx, 1
-	int 0x10
-	jmp .endl
-.return:
-	ret
-
-
-mvc:
-	mov ah, 0x3
-	mov bh, 0
+	mov bl, 9
 	int 0x10
 
-	inc dl
-	
-	mov ah, 0x2
-	int 0x10
-	inc si
-	ret
+	mov si, hello_str
+	call PrintString
 
-str: db "Hello, world!", 0
+	jmp InfiniteLoop
 
+PrintString:
+	jmp .loop
+	.loop:
+		lodsb
+		
+		cmp al, 0
+		je .return
 
-times 510-($-$$) db 0
-dw 0xAA55
+		;; Print character
+		mov ah, 0xE
+		mov bh, 0
+		mov bl, 0xF
+		int 0x10
+
+		;; Get cursor position (Column in DL)
+		mov ah, 0x3
+		mov bh, 0
+		int 0x10
+
+		;; Increment column
+		inc dl
+
+		;; Set new cursor position
+		mov ah, 0x2
+		mov bh, 0
+		int 0x10
+		
+		jmp .loop
+
+	.return:
+		ret
+		
+
+InfiniteLoop:
+	jmp $
+
+hello_str: db "Hello, World!", 0
+
+times 510 - ($ - $$) db 0
+db 0x55
+db 0xAA
+
